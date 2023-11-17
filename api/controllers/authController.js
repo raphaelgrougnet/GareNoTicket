13a81da8d65e5976f2a4d77eb2811c130b3f0a77
Raphaël Grougnet <raphaelgrougnet83000@gmail.com>
2023-11-17 10:22:49 -0500

    Ajout de la route d'authentification login

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -3,15 +3,68 @@ const jwt = require('jsonwebtoken');
 const config = require('../config');
 const User = require('../models/user');
 
-exports.login = (req, res, next) => {
+exports.login = async (req, res, next) => {
   const { email, password } = req.body;
-
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ message: 'Email ou Mot de passe incorrect!' });
+      }
+      bcrypt.compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            const token = jwt.sign(
+              { email: user.email, isValet : user.isValet, price : user.price, voiture: user.voiture, userId: user._id },
+              process.env.SECRET_JWT,
+              { expiresIn: '1h' }
+            );
+            res.status(200).json({ token: token });
+          } else {
+            res.status(401).json({ message: 'Email ou Mot de passe incorrect!' });
+          }
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json({ message: 'Une erreur s\'est produite!' });
+        });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'Une erreur s\'est produite!' });
+    });
 
 };
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
   const { email, username, password, confirmPassword } = req.body;
-
+  User.findOne({ email })
+    .then(userDoc => {
+      if (userDoc) {
+        return res.status(401).json({ message: 'Utilisateur déjà existant!' });
+      }
+      return bcrypt.hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email,
+            username,
+            password: hashedPassword,
+            isValet: false,
+            price: 0
+          });
+          return user.save();
+        })
+        .then(result => {
+          res.status(201).json({ message: 'Utilisateur créé!', userId: result._id });
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json({ message: 'Une erreur s\'est produite!' });
+        });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'Une erreur s\'est produite!' });
+    });
 
 };
 
