699752bb4b6f47c7c37261fce28051533e923443
Raphaël Grougnet <raphaelgrougnet83000@gmail.com>
2023-11-18 23:10:58 -0500

    ajout de la vue register

---
+++ frontend/src/views/RegisterView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex justify-center">
+      <RegisterForm />
+    </div>
+  </template>
+  
+  <script>
+      import RegisterForm from '../components/RegisterForm.vue'
+  
+      export default {
+      name: 'RegisterView',
+      components: {
+          RegisterForm
+      }
+      }
+  </script>
+  
+  <style scoped>
+  </style>
+  
