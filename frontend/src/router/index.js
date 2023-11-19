22df857119045e9ab056428c9c603d026311c7fe
Raphaël Grougnet <raphaelgrougnet83000@gmail.com>
2023-11-18 23:10:01 -0500

    ajout de la route register et methode beforeEnter

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,20 +7,17 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
-      beforeEnter: (to, from, next) => {
-        if (localStorage.getItem('token')) {
-          next();
-        } else {
-          next({ name: 'login' });
-        }
-        next()
-      }
+      component: HomeView
     },
     {
       path: '/login',
       name: 'login',
       component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue')
     },
     {
       path: '/logout',
@@ -30,7 +27,21 @@ const router = createRouter({
         next({ name: 'login' });
       }
     },
-  ]
+  ],
+  
+})
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token');
+
+  if (to.name !== 'login' && token === null) {
+    localStorage.removeItem('token');
+    next({ name: 'login' })
+  } else if (to.name === 'login' && token !== null) {
+    next({ name: 'home' }) 
+  } else {
+    next()
+  }
 })
 
 export default router
